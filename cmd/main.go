834397d486f692da55f1f5c19ddb96dd794eca0e
Hubert Kasprzycki <67634024+hhubert6@users.noreply.github.com>
2024-03-11 22:26:03 +0100

    Initial go project with ebiten engine

---
+++ cmd/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Hello!")
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 320, 240
+}
+
+func main() {
+	game := &Game{}
+
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Raycaster")
+
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
+}
